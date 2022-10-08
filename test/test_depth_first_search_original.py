4fca8dc5e842da68db79f09d3d0257ef2d63e5f4
NumberCruncher <physik.martin@gmail.com>
2022-10-08 14:51:26 +0200

    2022-10-08 data generation

--- test/test_depth_first_search_original.py
+++ test/test_depth_first_search_original.py
@@ -8,6 +8,7 @@ from plot.plotter import Plotter
 from shottky_dance.depth_first_search import DepthFirstSearch, DepthFirstSearch2
 from shottky_dance.depth_first_search_original import DepthFirstSearchOriginal, KissingSchottky, ApollonianGasket
 
+
 class depth_first_search_original_test(unittest.TestCase):
     def test_initialize(self):
         dfs = DepthFirstSearchOriginal(KissingSchottky, y=1, k=1, eps=0.1)
@@ -17,15 +18,15 @@ class depth_first_search_original_test(unittest.TestCase):
 
     def test_end_check(self):
         dfs = DepthFirstSearchOriginal(KissingSchottky, y=1, k=0.1, eps=0.1)
-        dfs.setup_start(begin_tag=[0,1,2,3],end_tag=[1,0,3,2])
-        print(dfs.check_end(test=[2,3,1]),dfs.breaking_length)
-        print(dfs.check_end(test=[1]),dfs.breaking_length)
-        print(dfs.check_end(test=[1,1]),dfs.breaking_length)
-        print(dfs.check_end(test=[1,0,3]),dfs.breaking_length)
-        print(dfs.check_end(test=[1,0,3,1]),dfs.breaking_length)
-        print(dfs.check_end(test=[1,0,3]),dfs.breaking_length)
-        print(dfs.check_end(test=[1,0,4]),dfs.breaking_length)
-        print(dfs.check_end(test=[1,0]),dfs.breaking_length)
+        dfs.setup_start(begin_tag=[0, 1, 2, 3], end_tag=[1, 0, 3, 2])
+        print(dfs.check_end(test=[2, 3, 1]), dfs.breaking_length)
+        print(dfs.check_end(test=[1]), dfs.breaking_length)
+        print(dfs.check_end(test=[1, 1]), dfs.breaking_length)
+        print(dfs.check_end(test=[1, 0, 3]), dfs.breaking_length)
+        print(dfs.check_end(test=[1, 0, 3, 1]), dfs.breaking_length)
+        print(dfs.check_end(test=[1, 0, 3]), dfs.breaking_length)
+        print(dfs.check_end(test=[1, 0, 4]), dfs.breaking_length)
+        print(dfs.check_end(test=[1, 0]), dfs.breaking_length)
 
     def test_kissing_schottky(self):
         dfs = DepthFirstSearchOriginal(KissingSchottky, y=0.5, k=0.1, eps=0.0001)
@@ -41,45 +42,62 @@ class depth_first_search_original_test(unittest.TestCase):
         plt.gca().set_aspect('equal')
         fig = plt.gcf()
         fig.set_size_inches(34, 14)
-        for i in range(100,200):
-            k=0.5-0.495*(i-100)/100
+        for i in range(100, 200):
+            k = 0.5 - 0.495 * (i - 100) / 100
             dfs = DepthFirstSearchOriginal(KissingSchottky, y=0.875, k=k, eps=0.001)
             dfs.run(close_curve=True)
             # plt.scatter(np.real(dfs.points), np.imag(dfs.points), s=0.5, marker='.')
             plt.clf()
-            plt.plot(np.real(dfs.points),np.imag(dfs.points))
-            fig.savefig("/home/jmartin/PycharmProjects/indras_pearls/tmp/image"+str(i)+".png",dpi=100)
-            #plt.show()
+            plt.plot(np.real(dfs.points), np.imag(dfs.points))
+            fig.savefig("/home/jmartin/PycharmProjects/indras_pearls/tmp/image" + str(i) + ".png", dpi=100)
+            # plt.show()
             # Plotter.plot(*dfs.circs) # only used this for level_max<6
 
     def test_sequence2(self):
         plt.gca().set_aspect('equal')
         fig = plt.gcf()
         fig.set_size_inches(34, 14)
-        for i in range(100,200):
-            y=3*(1-(i-100)/100)
+        for i in range(100, 200):
+            y = 3 * (1 - (i - 100) / 100)
             dfs = DepthFirstSearchOriginal(KissingSchottky, y=y, k=0.1, eps=0.001)
             dfs.run(close_curve=True)
             # plt.scatter(np.real(dfs.points), np.imag(dfs.points), s=0.5, marker='.')
             plt.clf()
-            plt.plot(np.real(dfs.points),np.imag(dfs.points))
-            fig.savefig("/home/jmartin/PycharmProjects/indras_pearls/tmp/figure2_"+str(i)+"_"+str(np.round(y*10)/10)+".png",dpi=100)
-            #plt.show()
-            #Plotter.plot(*dfs.circs) # only used this for level_max<6
+            plt.plot(np.real(dfs.points), np.imag(dfs.points))
+            fig.savefig("/home/jmartin/PycharmProjects/indras_pearls/tmp/figure2_" + str(i) + "_" + str(
+                np.round(y * 10) / 10) + ".png", dpi=100)
+            # plt.show()
+            # Plotter.plot(*dfs.circs) # only used this for level_max<6
 
     def test_simple(self):
         dfs = DepthFirstSearchOriginal(KissingSchottky, y=0.5, k=0.1, eps=0.001)
         dfs.run(close_curve=True)
         plt.clf()
-        plt.plot(np.real(dfs.points),np.imag(dfs.points))
+        plt.plot(np.real(dfs.points), np.imag(dfs.points))
         plt.gca().set_aspect('equal')
         plt.show()
         # Plotter.plot(*dfs.circs) # only used this for level_max<6
 
     def test_Apollonian(self):
-        dfs = DepthFirstSearchOriginal(ApollonianGasket,eps=0.05)
+        dfs = DepthFirstSearchOriginal(ApollonianGasket, eps=0.1)
         dfs.run(close_curve=True)
         plt.clf()
         plt.gca().set_aspect('equal')
-        plt.plot(np.real(dfs.points),np.imag(dfs.points))
+        plt.plot(np.real(dfs.points), np.imag(dfs.points))
         plt.show()
+
+    def test_generate_Apollonian_data(self):
+        eps = 0.1
+        while eps > 0.00001:
+            dfs = DepthFirstSearchOriginal(ApollonianGasket, eps=eps)
+            dfs.run(close_curve=True)
+            fp = open(r'/home/jmartin/blendermath/video_apollonian/data/apollonian_' + str(eps) + '.dat', 'w')
+            first = True
+            for p in dfs.points:
+                if not first:
+                    fp.write(",")
+                else:
+                    first = False
+                fp.write("%s" % p)
+            fp.close()
+            eps /= 10
